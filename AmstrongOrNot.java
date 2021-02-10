88592334c0b21513361ab784cfddb7200dd2a822
AbhijithCR-1 <78850178+AbhijithCR-1@users.noreply.github.com>
2021-02-10 16:14:49 +0530

    Add files via upload

---
+++ AmstrongOrNot.java
@@ -0,0 +1,19 @@
+
+public class AmstrongOrNot {
+	public static void main(String[] args) {
+        int num = 371, res= 0, rem, a;
+        a = num;
+        while( num != 0 )
+        {
+            rem = num % 10;
+            res = res + rem*rem*rem;
+            num  /= 10;
+        }
+
+        if (a == res)
+            System.out.println(a + " is a amstrong .");
+        else
+            System.out.println(a + " is not a amstrong.");
+    }
+}
+
